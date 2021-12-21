bf7245305c86d0017b95fd31d50758bac1cf6d35
erhan11 <77719565+erhan11@users.noreply.github.com>
2021-12-21 12:08:31 +0200

    finishing

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import About from "./components/About/About";
 import Product from "./components/product/Product";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
-
+import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
+import React, { useContext }  from 'react';
 
 const App = () => {
-  return <div>
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode; 
+  return (
+  <div style={{backgroundColor:darkMode ? "#222" : "white", color: darkMode && "white" }}>
+      <Toggle/>
   <Intro/>
   <About/>
   <ProductList/>
   <Product/>
   <Contact/>
-  </div>;
-
+  </div>
+);
 };
 
 export default App;
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,12 +2,31 @@ import Phone from "../../img/phone.jpeg";
 import Email from "../../img/phone.jpeg";
 import Address from "../../img/address.png";
 import "./contact.css"
+import { useRef } from "react"
+import emailjs from 'emailjs-com';
+import { useState } from 'react';
 
-function Contact() {
-  return (
-    <div className="c">
+
+
+const Contact = () => {
+    const formRef = useRef()
+const [done, setDone] = useState(false)
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        emailjs.sendForm('service_ntrzo6l', 'template_5oglupq', formRef.current, 'user_hYWSlP6ia5W9Gby3XOPxu')
+        .then((result) => {
+            console.log(result.text);
+            setDone(true)
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+    
+    return (
+
+<div className="c">
       <div className="c-bg"></div>
-      <div className="wrapper">
+      <div className="c-wrapper">
         <div className="c-left">
           <h1 className="c-title">Lets discus your project</h1>
           <div className="c-info">
@@ -33,12 +52,13 @@ function Contact() {
             <p className="c-desc">
                 <b>Whats your story?</b>
             </p>
-            <form>
+            <form ref={formRef} onSubmit={handleSubmit}>
             <input type="text" placeholder="Name" name="user_name" />
             <input type="text" placeholder="Subject" name="user_subject" />
             <input type="text" placeholder="Email" name="user_email" />
             <textarea rows="5" placeholder="Message" name="message"></textarea>
            <button>Submit</button>
+           {done && "Mesajınız için teşekkür ederiz."}
             </form>
         </div>
       </div>
